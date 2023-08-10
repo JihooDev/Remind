bd3cdb23b6b937f4e82243ba0942d7c72443b454
jihooDev <wlgn829@kakao.com>
2023-08-11 08:21:49 +0900

    Fix: 선택 메뉴 만들기

--- src/components/FIlterBar.js
+++ src/components/FIlterBar.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { styled } from 'styled-components'
 import { ht, wt } from '../../responsive/responsive'
+import CustomText from './CustomText';
+import { COLORS } from '../asset/colors';
 
 const FIlterBar = ({
     data,
@@ -9,17 +11,53 @@ const FIlterBar = ({
 }) => {
 
     const [menuStatus, setMenuStatus] = useState('최신순');
+    const [selectStatus, setSelectStatus] = useState('선택');
+    const tabMenuList = [
+        {
+            id: 1,
+            title: selectStatus,
+        },
+        {
+            id: 2,
+            title: menuStatus
+        }
+    ]
 
     return (
-        <FilterView></FilterView>
+        <FilterView>
+            {
+                tabMenuList.map(({ id, title }) => (
+                    <FilterButton
+                        activeOpacity={.9}
+                    >
+                        <CustomText
+                            text={title}
+                            color={COLORS.white}
+                        />
+                    </FilterButton>
+                ))
+            }
+        </FilterView>
     )
 }
 
 const FilterView = styled.View`
     width: 100%;
     height: ${ht(250)}px;
-    background-color: red;
     padding: 0 ${wt(50)}px;
+    margin-top: ${ht(80)}px;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+const FilterButton = styled.TouchableOpacity`
+    width: ${wt(350)}px;
+    height: ${ht(130)}px;
+    background-color: red;
+    justify-content: center;
+    align-items: center;
+    background-color: ${props => props.status ? COLORS.red : COLORS.success};
+    border-radius: 5px;
 `
 
 export default FIlterBar
